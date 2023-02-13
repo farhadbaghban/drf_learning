8d5bf47cd0c22a30e8e427404b5f24af1ffdf751
farhadbaghban <baghbanfarhad@gmail.com>
2023-02-13 19:26:42 +0330

    request response Serializer intiated

--- home/views.py
+++ home/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-from django.views import View
+from rest_framework.views import APIView
 from django.contrib import messages
+from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
 
 
-class HomeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "home/index.html")
+class HomeView(APIView):
+    """
+    first step : without query_params
+
+    secend : with query params
+
+    third : recieve data with body
+
+    fourth : using Serializer
+    """
+
+    # def get(self, request, *args, **kwargs):
+    #     # return Response({"name": "farhad"})
+    #     # name = request.GET["name"]
+    #     name = request.query_params["name"]
+    #     return Response({"name": name})
+
+    # def post(self, request, *args, **kwargs):
+    #     name = request.data["name"]
+    #     return Response({"name": name})
+
+    def get(self, requset, *args, **kwargs):
+        persons = Person.objects.all()
+        ser_data = PersonSerializer(instance=persons, many=True)
+        return Response(data=ser_data.data)
