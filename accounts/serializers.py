c2068cdca22f9f48aac902243c321d53819af0de
farhadbaghban <baghbanfarhad@gmail.com>
2023-02-14 17:21:03 +0330

    Model Serializers added

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,16 +2,35 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
+"""
+    first = strat serializer
+
+    second = user model serializer
+"""
+
+
 def clean_email(value):
     if "admin" in value:
         raise serializers.ValidationError("you cant use admin in email")
 
 
-class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True, validators=[clean_email])
-    password = serializers.CharField(required=True, write_only=True)
+# class UserRegisterSerializer(serializers.Serializer):
+#     username = serializers.CharField(required=True)
+#     email = serializers.EmailField(required=True, validators=[clean_email])
+#     password = serializers.CharField(required=True, write_only=True)
+#     password2 = serializers.CharField(required=True, write_only=True)
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password", "password2")
+        extra_kwargs = {
+            "email": {"validators": [clean_email]},
+            "password": {"write_only": True},
+        }
 
     def validate(self, data):
         if data["password"] != data["password2"]:
